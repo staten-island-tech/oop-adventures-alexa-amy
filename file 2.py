40123cfb24c6159b562650e881a8de0a190d0a05
AmyChan26 <124072374+AmyChan26@users.noreply.github.com>
2023-05-08 10:58:35 -0400

    Update file 2.py

--- file 2.py
+++ file 2.py
@@ -1,3 +1,3 @@
-print("The wife ran through the park, her shoes cracking on the ground. A loud pair of leather shoes roam behind her, growing louder with every step. ")
 
-Continue == print("Press c to continue")
+
+
