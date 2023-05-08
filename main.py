1c531cbb2ccc1e747f39fba4f873595e00e62fb2
alexavso <alexasolanou@gmail.com>
2023-05-08 10:50:02 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+class Player(User):
+    def __init__(self, id, name, coins):
+        super().__init__(id,name)
+        self.coins = coins
+    def __str__(self):
+        return f"{self.name}, {self.coins}, {self.id}"
+
