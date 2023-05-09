24ffc5f11f39526ad13551fecc887d56ec41bb51
alexavso <alexasolanou@gmail.com>
2023-05-09 10:48:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,5 @@ class Player(User):
         super().__init__(id,name)
         self.coins = coins
     def __str__(self):
-<<<<<<< HEAD
         return f"{self.name}, {self.coins}"
-=======
-        return f"{self.name}, {self.coins}, {self.id}"
-    
-
->>>>>>> d8958422bcc386a777663c48be25de452ac2f6cc
 
