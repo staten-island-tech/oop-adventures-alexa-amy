8839e2bacc92f761f4578c85f08a358fd726e6e5
AmyChan26 <124072374+AmyChan26@users.noreply.github.com>
2023-05-10 10:57:17 -0400

    Update items.py

--- items.py
+++ items.py
@@ -1,1 +1,10 @@
-class Items:
+class items: 
+     def __init__(self, name, amount):
+        self.name = name
+        self.amount = amount 
+   
+class Ability_Shield(items):
+    def __int__(self, name, amount):
+        super().__int__(name, amount)
+    def __str__(items):
+        return f"{items.name}, {items.amount}"
