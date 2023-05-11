0793d72594da6cc2215653e33a00bf15d5a879bb
AmyChan26 <124072374+AmyChan26@users.noreply.github.com>
2023-05-11 10:59:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 class User:
-    def __init__(self, id, name, pokemon):
+    def __init__(self, id, name):
         self.id = id
         self.name = name
-        self.pokemon = pokemon
 
 class Player(User):
     def __init__(self, id, name, coins):
@@ -13,16 +12,22 @@ class Player(User):
 
 class NPC(User):
     def __int__(self, name, trainer):
-        super(). __int__(name, trainer, pokemon)
+        super(). __int__(name, trainer )
+
+
+
 
 pokemon = []
 add_more_pokemon = "Y"
 while add_more_pokemon == "Y":
     user_request = input("Would you like to add another pokemon? ")
-    if user_request.upper() == "Y":
+    if user_request.upper() == "Y": 
         pokemon = input("Enter name of a pokemon ")
     else:
         print("Something went wrong, are you sure you typed the request correctly? ")
         continue  
     still_continue = input("Would you like to continue? Y/N: ")
     add_more_pokemon = still_continue
+print('your team '
+
+
