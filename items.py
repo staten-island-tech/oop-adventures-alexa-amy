8c48c3a9da8c695b44c32fc7167a4d4a9256cbd9
AmyChan26 <124072374+AmyChan26@users.noreply.github.com>
2023-05-12 10:54:58 -0400

    Update items.py

--- items.py
+++ items.py
@@ -15,8 +15,30 @@ class Heal_Potion(items):
     def __str__(items):
         return f"{items.name}, {items.amount}"
 
-class lucky_Egg(items):
+class Lucky_Egg(items):
     def __int__(self, name, amount):
         super().__int__(name,amount)
     def __str__(items):
         return f"{items.name}, {items.amount}"
+
+class Pokeball(items):
+    def __int__(self, name, amount):
+        super().__int__(name,amount)
+    def __str__(items):
+        return f"{items.name}, {items.amount}"
+
+class food:
+    def __int__(self, name, amount):
+        self.name = name 
+        self.amount = amount 
+
+class Berries:
+    def __int__(self, name, amount):
+        super().__int__(name, amount)
+    def __str__(items):
+        return f"{items.name}, {items.amount}"
+
+
+
+    
+
