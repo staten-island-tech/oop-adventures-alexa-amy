27d082caca5caecb35513a4e01f4a9f70a84739a
alexavso <alexasolanou@gmail.com>
2023-05-19 15:22:41 -0400

    Update storyline.py

--- storyline.py
+++ storyline.py
@@ -92,29 +92,27 @@ if Forest_Request in r:
     print("-------\nYou enter the Flourish Forest and see many wild pokemon surrounding the area..")
     time.sleep(2)
     print("You look around the area and notice there is a pond ahead, and a cave.")
+    time.sleep(1.5)
     user_request2 = input("Would you like to go to the pond or the cave? " )
     if user_request2 == "Pond" or "pond":
         print("-------\nYou come across a wild Squirtle and Psyduck.")
         user_request3 = input("Do you want to attempt at catching one? Y/N: ")
-    
-
-if user_request3 == "Y" or "Yes":
+    if user_request3 in r:
         user_request4 = input("Which one do you want to catch? ")
-
-if user_request4 == "Squirtle" or "squirtle":
-    pokeball_request = input("Pick a pokeball: Pokeball or Greatball: ")
+    if user_request4 == "Squirtle" or "squirtle":
+        pokeball_request = input("Pick a pokeball: Pokeball or Greatball: ")
     if pokeball_request == "Pokeball":
         print("-------\nYou now have 14x Pokeballs remaining")
     else:
         print("------\nYou now have 4x Greatballs remaining")
-        print("You throw your", {pokeball_request}, "at Squirtle, but miss.")
+    print("You throw your", {pokeball_request}, "at Squirtle, but miss.")
     user_request5 = input("Do you wish to try again? Y/N: ")
     if user_request5 == "Y" or "Yes":
         user_request6 = input("Pick your Pokeball: ")
-        if pokeball_request == "Pokeball":
-            print("-------\nYou now have 13x Pokeballs remaining")
-        else:
-            print("------\nYou now have 3x Greatballs remaining")
+    if pokeball_request == "Pokeball":
+        print("-------\nYou now have 13x Pokeballs remaining")
+    else:
+        print("------\nYou now have 3x Greatballs remaining")
 else:
     print("You must enter because I said so :)")
     continue1 = input("Would you like to continue? Y/N: ")
