6b0c6fb1e10feecd289fe6c55c5635dc7e3e90bb
alexavso <alexasolanou@gmail.com>
2023-05-22 18:45:39 -0400

    Update storyline.py

--- storyline.py
+++ storyline.py
@@ -1,35 +1,3 @@
-# You are a student in Mr. Whalen's pokemon training center. 
-# In order to graduate you must beat him in a pokemon battle.
-# However you don't have any pokemon to begin with.
-# Go into the forest to search for some pokemon.
-
-# Here is a gift to help you:
-# 15x Pokeball, 5x Greatball, and 5x Sitrus Berry!
-
-# Good luck on your journey!
-
-# ------
-
-# Would you like to enter the forest? Y/N
-# Y: You enter the forest, wild pokemon surround the area..
-# N: 
-# You look around your area and notice there is a pond ahead, and a cave.
-# Would you like to go to the pond or the cave?
-# Pond: You come across a wild Squirtle and Psyduck.
-    # Do you want to attempt at catching one? Y/N
-    # N: You run away from the area and go into the cave instead.
-    # Y: Which one do you want to catch?
-        # Pick your pokeball: 
-            # You now have 4x Greatball
-            # You now have 14x Pokeball
-    # Squirtle: You throw your {pokeball} at Squirtle, but miss.
-    # Squirtle: Do you wish to try again? Y/N
-        # Y: Pick your pokeball:
-        # You now have 3x Greatball
-        # You now have 13x Pokeball
-    # Squirtle: You throw your {pokeball} at Squirtle and successfully catch it!
-    # Squirtle: Would you like to name it?
-
 import time
 
 print("-------\nYou are a student in Mr. Whalen's pokemon training center.")
