059518adb70cc232b6ebf091b6418c492e77e617
AmyChan26 <124072374+AmyChan26@users.noreply.github.com>
2023-05-22 10:58:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
-import random
+x=1 
+y= 2
+z = 3
 class User:
     def __init__(self, id, name):
         self.id = id
@@ -17,11 +19,11 @@ class NPC(User):
         super(). __int__(name, trainer )
 
 print('Hello trainee, what a thrilling day to be out here with your battles ')
-time.sleep(2)
+time.sleep(y)
 user_input = input('Well then, want to battle it out to earn EXP? ')
 if user_input.upper() == "Y":
     print("Okay, I will let you prepare first")
-    time.sleep(2)
+    time.sleep(y)
     print("Enter 3 pokemons to battle")
 pokemon = []
 add_more_pokemon = "Y"
@@ -35,38 +37,38 @@ while add_more_pokemon == "Y":
     add_more_pokemon = still_continue
     pokemon.append(pokemon_r)
 print(f"your team consists of: {pokemon}")
-time.sleep(2)
+time.sleep(y)
 user_request = input("Ready to battle Mr.Whalen? Y/N ")
 if user_request.upper() == "Y":
     print("Ready when you are ")
 else: 
     print("Well, I'm ready so you are now ") #hold 
 
-time.sleep(2)
+time.sleep(y)
 team = [pokemon]
 switch = [pokemon]
 Battles = 0 
 user_health = 100 
 attack_request = "1", "2", "3", "4"
 while (Battles <= 1):
-    random_pokemon = (random.randint("primeape, snorlax"))
+    random_pokemon = (random.randint(1,5))
     attack_request = input(f"Choose your move Trainee: 1) attack Mr.Whalen's {random_pokemon} 2) Abide 3) healing 4)Switch to {pokemon} ")
     if attack_request == "1":
         print(random.randint(choice_1, choice_2))
         choice_1 = print("Good job! you Mr.Whalen's pokemon hp dropped by 5 ")
-        time.sleep(2)
+        time.sleep(y)
         choice_2 = print("Missed AHHAHAHA ")
     if attack_request == "2": 
         print("Move wasted ")
-        time.sleep(1)
+        time.sleep(z)
     if attack_request == "3": 
         print("Your haling potion succeed and your pokemon gains 5 hp ")
-        time.sleep(2)
+        time.sleep(y)
         print("Your healing potion failed, Mr.Whalen's turn now ")
-        time.sleep(2)
+        time.sleep(y)
     if attack_request == "4": 
         print(f"Which pokemon do you want to use: {team}")
-        time.sleep(3)
+        time.sleep(z)
         attack_request = print(f"Detected, now switching to {switch}")
 else:
     print("Are you sure you chose a correct option? Enter again ")
