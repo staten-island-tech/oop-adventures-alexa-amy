66de6d64487e9c107aa380be0a8fe21d53992692
alexavso <alexasolanou@gmail.com>
2023-05-23 10:37:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import time
+<<<<<<< HEAD
 import random
+=======
+x=1 
+y= 2
+z = 3
+class User:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+class Player(User):
+    def __init__(self, name, coins):
+        super().__init__(id,name)
+        self.coins = coins
+    def __str__(self):
+        return f"{self.name}, {self.coins}"
+
+class NPC(User):
+    def __int__(self, name, trainer):
+        super(). __int__(name, trainer )
+>>>>>>> 50e49b80716b72007137d4c0b1103529f9031cfc
 
 print('Hello trainee, what a thrilling day to be out here with your battles ')
-time.sleep(2)
+time.sleep(y)
 user_input = input('Well then, want to battle it out to earn EXP? ')
 if user_input.upper() == "Y":
     print("Okay, I will let you prepare first")
-    time.sleep(2)
+    time.sleep(y)
     print("Enter 3 pokemons to battle")
 pokemon = []
 add_more_pokemon = "Y"
@@ -19,8 +40,13 @@ while add_more_pokemon == "Y":
     still_continue = input("Would you like to continue? Y/N: ")
     add_more_pokemon = still_continue
     pokemon.append(pokemon_r)
+<<<<<<< HEAD
 print(f"your team consists of:",pokemon)
 time.sleep(2)
+=======
+print(f"your team consists of: {pokemon}")
+time.sleep(y)
+>>>>>>> 50e49b80716b72007137d4c0b1103529f9031cfc
 user_request = input("Ready to battle Mr.Whalen? Y/N ")
 if user_request.upper() == "Y":
     print("Ready when you are ")
@@ -29,32 +55,45 @@ else:
 
 #hold 
 
-time.sleep(2)
+time.sleep(y)
 team = [pokemon]
+switch = [pokemon]
 Battles = 0 
 user_health = 100 
 attack_request = "1", "2", "3", "4"
+<<<<<<< HEAD
 if (Battles <= 1):
     random_pokemon = (random.randint(1,5))
     attack_request = input(f"Choose your move Trainee: 1) attack Mr.Whalen's",random_pokemon, "2) Abide 3) healing 4)Switch to {pokemon}")
+=======
+pchoice= ["Primeape" ,"Snorlax"]
+while (Battles <= 1):
+    print(random.choice(pchoice))
+    attack_request = input(f"Choose your move Trainee: 1) attack Mr.Whalen's {pchoice} 2) Abide 3) healing 4)Switch to {pokemon} ")
+>>>>>>> 50e49b80716b72007137d4c0b1103529f9031cfc
     if attack_request == "1":
-       
-        random.randint((1,2))
+        print(random.randint(choice_1, choice_2))
         choice_1 = print("Good job! you Mr.Whalen's pokemon hp dropped by 5 ")
-        time.sleep(2)
+        time.sleep(y)
         choice_2 = print("Missed AHHAHAHA ")
     if attack_request == "2": 
         print("Move wasted ")
-        time.sleep(1)
+        time.sleep(z)
     if attack_request == "3": 
         print("Your haling potion succeed and your pokemon gains 5 hp ")
-        time.sleep(2)
+        time.sleep(y)
         print("Your healing potion failed, Mr.Whalen's turn now ")
-        time.sleep(2)
+        time.sleep(y)
     if attack_request == "4": 
+<<<<<<< HEAD
         print(f"Which pokemon do you want to use:",team)
         time.sleep(5)
         attack_request.upper = print(f"Detected, now switching to",team)
+=======
+        print(f"Which pokemon do you want to use: {team}")
+        time.sleep(z)
+        attack_request = print(f"Detected, now switching to {switch}")
+>>>>>>> 50e49b80716b72007137d4c0b1103529f9031cfc
 else:
     print("Are you sure you chose a correct option? Enter again ")
     still_continue = input("What attack method do you want? ")
@@ -62,11 +101,11 @@ else:
     
 if (user_health) == 0:
     print("Mr.Whalen: HAAHWAH I WON! ")
-    time.sleep(1)
+    time.sleep(x)
     print("Better next time, trainee ")
 
 if (Battles >= 1):
     print("Mr.Whalen: Oh man you beat my pokemons, see ya next time trainee! ")
-    time.sleep(1)
+    time.sleep(x)
     print("Good job trainee! Until next time, your next adventure awaits ")
 
