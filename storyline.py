d8a6042aa92cb7808a8669e059fd442965b8e19e
AmyChan26 <124072374+AmyChan26@users.noreply.github.com>
2023-05-23 10:49:11 -0400

    Update storyline.py

--- storyline.py
+++ storyline.py
@@ -1,28 +1,31 @@
 import time
+a = 1
+b = 1.5
+c = 2
 
 print("-------\nYou are a student in Mr. Whalen's pokemon training center.")
-time.sleep(2)
+time.sleep(c)
 print("In order to graduate you must beat him in a pokemon battle.")
-time.sleep(2)
+time.sleep(c)
 print("However you don't have any pokemon to begin with.")
-time.sleep(2)
+time.sleep(c)
 print("Go into the forest to search for some pokemon.")
-time.sleep(2)
+time.sleep(c)
 print("-------\nHere is a gift to help you:")
-time.sleep(1.5)
+time.sleep(b)
 print("15x Pokeball, 5x Greatball, and 5x Sitrus Berry!")
-time.sleep(2)
+time.sleep(c)
 print("Good luck on your journey!")
-time.sleep(2)
+time.sleep(c)
 
 r = ["Y", "y", "Yes", "yes"]
 Forest_Request = input("-------\nWould you like to enter Flourish Forest? Y/N: ")
 if Forest_Request in r:
-    time.sleep(1)
+    time.sleep(a)
     print("-------\nYou enter the Flourish Forest and see many wild pokemon surrounding the area..")
-    time.sleep(2)
+    time.sleep(c)
     print("You look around the area and notice there is a pond ahead, and a cave.")
-    time.sleep(1.5)
+    time.sleep(b)
     user_request2 = input("Would you like to go to the pond or the cave? " )
     if user_request2 == "Cave" or "cave":
         from Cave import *
