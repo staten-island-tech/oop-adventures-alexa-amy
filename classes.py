6f6c2ec3d9b70531c6aefb30cd101aa06816f4aa
alexavso <alexasolanou@gmail.com>
2023-05-24 10:59:17 -0400

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, name, inventory, coins):
+        self.name = input("Hello trainer! What's your name?: ")
+        self.inventory = inventory
+        self.coins = coins
