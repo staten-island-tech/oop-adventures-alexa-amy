1c5fe52f9afc70ee45a04ccc753cb4f39c1bba1f
alexavso <alexasolanou@gmail.com>
2023-05-24 22:27:05 -0400

    Update Pond.py

--- Pond.py
+++ Pond.py
@@ -1,4 +1,41 @@
 import time
+#new
+
+r = ["Y", "y", "Yes", "yes"]
+g = ["N", "No", "n", "no"]
+squirtle = ["Squirtle", "squirtle"]
+psyduck = ["Psyduck", "psyduck"]
+pokeball = ["Pokeball", "pokeball"]
+x = True
+inventory = []
+
+def Pond():
+    time.sleep(1.5)
+    print("As you walk closer, you come across a wild Squirtle and Psyduck.")
+    user_request3 = input("Do you want to attempt at catching one? Y/N: ")
+    if user_request3 in r:
+        choice1 = input("Which one do you want to catch? ")
+        print(choice1)
+        if choice1 in squirtle:
+            from Squirtle import Squirtle
+        else:
+            return Pond()
+    if user_request3 in g:
+        choice2 = input("Return to Cave? Y/N: ")
+        if choice2 in r:
+            from Cave import Cave
+        else:
+            return Pond()
+    else:
+        x = True    
+        while x == True:
+            q = input("Looks like something went wrong. Would you like to continue? Y/N: ")
+            if (q== "N" or q== "No" or q=="n" or q=="no"):
+                x = False
+                break
+Pond()
+
+""" #old
 a = 1
 b = 1.5
 c = 2
@@ -66,4 +103,4 @@ def Pond():
             q = input("It seems you made a mistake. Please try again: ")
             if q in r:
                 x = False
-Pond()
+Pond() """
