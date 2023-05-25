62f697d431fd2b933e8d47702a544a25b2d179c4
alexavso <alexasolanou@gmail.com>
2023-05-24 23:08:11 -0400

    Update storyline.py

--- storyline.py
+++ storyline.py
@@ -19,18 +19,22 @@ print("Good luck on your journey!")
 time.sleep(c)
 
 r = ["Y", "y", "Yes", "yes"]
-Forest_Request = input("-------\nWould you like to enter Flourish Forest? Y/N: ")
+c = ["Cave", "cave"]
+Forest_Request = input("-------\nWould you like to enter Eterna Forest? Y/N: ")
 if Forest_Request in r:
-    time.sleep(a)
-    print("-------\nYou enter the Flourish Forest and see many wild pokemon surrounding the area..")
-    time.sleep(c)
+    time.sleep(1)
+    print("-------\nYou enter the Eterna Forest and see many wild pokemon surrounding the area..")
+    time.sleep(2)
     print("You look around the area and notice there is a pond ahead, and a cave.")
-    time.sleep(b)
-    user_request2 = input("Would you like to go to the pond or the cave? " )
-    if user_request2 == "Cave" or "cave":
-        from Cave import Cave
+    time.sleep(1.5)
+    user_request2 = input("Would you like to go to the pond or the cave? ")
+    if user_request2 in c:
+        from Cave import Cave_Option
+        from PokemonRefresh import *
+        PokemonRefresh = Pokemon_Refresh
     else:
-        from Pond import Pond
+        from Pond import Pond_Option
+        from PokemonRefresh import Pokemon_Refresh
 else:
     print("You must enter because I said so :)")
     continue1 = input("Would you like to continue? Y/N: ")
