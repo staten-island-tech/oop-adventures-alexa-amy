292c30c0d882da696b5ade2698a559b772f335b2
alexavso <alexasolanou@gmail.com>
2023-05-24 22:27:03 -0400

    Create Psyduck.py

---
+++ Psyduck.py
@@ -0,0 +1,43 @@
+import time
+
+r = ["Y", "y", "Yes", "yes"]
+g = ["N", "No", "n", "no"]
+squirtle = ["Squirtle", "squirtle"]
+psyduck = ["Psyduck", "psyduck"]
+pokeball = ["Pokeball", "pokeball"]
+greatball = ["Greatball", "greatball"]
+cave_return = ["Cave", "cave"]
+x = True
+inventory = []
+
+def Psyduck():
+    pokeball_request = input("Pick a pokeball: Pokeball or Greatball: ")
+    if pokeball_request in pokeball:
+        print("-------\nYou now have 14x Pokeballs remaining")
+        print("-------\nYou throw your Pokeball at Psyduck, but miss.")
+    if pokeball_request in greatball:
+        print("------\nYou now have 4x Greatballs remaining")
+        print("-------\nYou throw your Greatball at Psyduck, but miss.")
+    else:   
+        while x == True:
+            q = input("Looks like something went wrong. Would you like to continue? Y/N: ")
+            if (q== "N" or q== "No" or q=="n" or q=="no"):
+                x = False
+                break
+    
+    #did not fix yet
+    user_request5 = input("Do you wish to try again? Y/N: ")
+    if user_request5 in r:
+        user_request6 = input("Pick your Pokeball: ")
+        if user_request6 in pokeball:
+            print("-------\nYou now have 13x Pokeballs remaining")
+            print("You throw your Pokeball at Psyduck..")
+        else:
+            print("------\nYou now have 3x Greatballs remaining")
+            print("You throw your Greatball at Psyduck..")
+        time.sleep(1.5)
+        print("...")
+        time.sleep(1.5)
+        print("You have successfully captured Psyduck!")
+        inventory.append("Psyduck")
+Psyduck()
