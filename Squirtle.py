870fc2aa9cd50e7cae888c9c4ac55fcdd196d092
alexavso <alexasolanou@gmail.com>
2023-05-24 23:08:13 -0400

    Update Squirtle.py

--- Squirtle.py
+++ Squirtle.py
@@ -7,45 +7,47 @@ psyduck = ["Psyduck", "psyduck"]
 pokeball = ["Pokeball", "pokeball"]
 greatball = ["Greatball", "greatball"]
 cave_return = ["Cave", "cave"]
-x = True
 inventory = []
+x = True
+y = True
 
 def Squirtle():
-    pokeball_request = input("Pick a pokeball: Pokeball or Greatball: ")
+    pokeball_request = input("Pick a pokeball - Pokeball or Greatball: ")
     if pokeball_request in pokeball:
         print("-------\nYou now have 14x Pokeballs remaining")
         print("-------\nYou throw your Pokeball at Squirtle, but miss.")
-    if pokeball_request in greatball:
+    elif pokeball_request in greatball:
         print("------\nYou now have 4x Greatballs remaining")
         print("You throw your Greatball at Squirtle, but miss.")
     else:   
         while x == True:
             q = input("Looks like something went wrong. Would you like to continue? Y/N: ")
-            if (q== "N" or q== "No" or q=="n" or q=="no"):
+            if (q== "Y" or q== "Yes" or q=="y" or q=="yes"):
                 x = False
                 break
-    user_request5 = input("Do you wish to try again? Y/N: ")
+    
+    user_request5 = input("Do you wish to catch Squirtle again? Y/N: ")
     if user_request5 in r:
-        user_request6 = input("Pick your Pokeball: ")
+        user_request6 = input("Pick your Pokeball - Pokeball or Greatball: ")
         if user_request6 in pokeball:
             print("-------\nYou now have 13x Pokeballs remaining")
-        else:
+            print("-------\nYou throw your Pokeball at Squirtle..")
+        if user_request6 in greatball:
             print("------\nYou now have 3x Greatballs remaining")
+            print("You throw your Greatball at Squirtle..")
+
     if user_request5 in g:
         choice2 = input("Want to catch psyduck instead or go to the cave?\nType 'Psyduck' for Psyduck or 'Cave' for Cave: ")
         if choice2 in psyduck:
-            from Psyduck import Psyduck
+            from Psyduck import Psyduck_Option
         else:
+            print("Returning to Squirtle..")
             return Squirtle()
         if choice2 in cave_return:
-            from Cave import Cave
+            from Cave import Cave_Option
         else:
+            print("Returning to Squirtle..")
             return Squirtle()
-    
-    #did not fix yet
-    else:
-        user_request4 = input("Which one do you want to catch? ")    
-    print("You throw your",user_request6, "at Squirtle..")
     time.sleep(1.5)
     print("...")
     time.sleep(1.5)
