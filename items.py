e0d86b1417c50f118018cc11a8b3a68514090fb1
alexavso <alexasolanou@gmail.com>
2023-06-06 10:59:47 -0400

    Update items.py

--- items.py
+++ items.py
@@ -27,12 +27,12 @@ class Pokeball(items):
     def __str__(items):
         return f"{items.name}, {items.amount}"
 
-class food:
+class food(items):
     def __int__(self, name, amount):
         self.name = name 
         self.amount = amount 
 
-class Berries:
+class Berries(items):
     def __int__(self, name, amount):
         super().__int__(name, amount)
     def __str__(items):
