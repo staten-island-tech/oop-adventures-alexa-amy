c33134871273c0a9570e650f11147755310eaafa
alexavso <alexasolanou@gmail.com>
2023-06-09 00:25:36 -0400

    Update battle.py

--- battle.py
+++ battle.py
@@ -3,13 +3,10 @@ import random
 
 r = ["Y", "y", "Yes", "yes"]
 g = ["N", "No", "n", "no"]
-pokemon = []
-team = [pokemon]
 op1 = ["Good job! Mr.Whalen's pokemon hp dropped by 30", "Missed AHHAHAHA"]
 op3 = ["Your healing potion succeeded\nYour pokemon gains 5 hp", "Your healing potion failed, Mr.Whalen's turn now"]
 op4 = ["Battle", "battle", "BATTLE"]
 op5 = ["Shop", "shop", "SHOP"]
-attack_request = "1", "2", "3", "4"
 
 def Battle():
     user_health = 100
@@ -27,7 +24,7 @@ def Battle():
                 user_health = user_health - 40
                 time.sleep(1.5)
                 print("Your Pokemon health",user_health)
-            else:
+            if random.choice(op1) == "Missed AHHAHAHA":
                 print("Mr. Whalen uses bite")
                 user_health = user_health - 25
                 time.sleep(1.5)
@@ -42,7 +39,7 @@ def Battle():
         
         if attack_request == "3": 
             print(random.choice(op3))
-            if random.choice(op3) == "Your healing potion succeeded\nYour pokemon gains 5 hp":
+            if random.choice(op3) == "Your healing potion succeeded\nYour pokemon gains 10 hp":
                 user_health = user_health + 10
                 time.sleep(1.5)
                 print("Your Pokemon health",user_health)
@@ -52,8 +49,12 @@ def Battle():
                 print("Mr.Whalen uses Rollout\nYour Pokemon hp dropped by 30")
                 time.sleep(1.5)
                 print("Your Pokemon health is",user_health)
-    
-        if(user_health) == 0:
+        
+        if w_health == 0:
+            print("You beat Mr. Whalen! Congrats")
+            break
+        
+        if user_health < 0:
             print("Mr.Whalen wins!")
             time.sleep(1.5)
             battle_again = input("-------\nWould you like to battle Mr.Whalen again or go to the shop?\n'Battle' to try again or 'Shop' to go to the shop: ")
@@ -62,12 +63,7 @@ def Battle():
             elif battle_again in op5:
                 from shop import Go_Shop
                 print(Go_Shop)
-        
-        if(w_health) == 0:
-            print("You beat Mr. Whalen! Congrats")
-            w_health = False
-            break
-    
+
     else:
         print("Are you sure you chose a correct option? Enter again: ")
         still_continue = input("What attack method do you want to use? ")
