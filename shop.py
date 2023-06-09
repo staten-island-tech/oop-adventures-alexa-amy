28b969a103d86611d82082a724f13d670a8294eb
alexavso <alexasolanou@gmail.com>
2023-06-09 00:25:29 -0400

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,8 +1,5 @@
 import time 
-from items import items
-x = 1
-y = 2 
-z = 3
+
 coins = 100 
 Y = ["Y", "y", "Yes", "yes"]
 N = ["N", "n", "No", "no"]
@@ -15,31 +12,30 @@ Pokeball = ["Pokeball", "pokeball" , "6"]
 
 def Go_Shop():
     print("-------\nThis is the Go Shop, life of paradise for the need of your pokemon(s) ")
-    time.sleep(y)
+    time.sleep(1.5)
     user_request1 = input("Would you like to purchase fellow Trainee? ")
-    time.sleep(y)
+    time.sleep(1.5)
     if user_request1 in Y:
-        print(" The items in stock are listed ")
-        print(" 1)  Ability Shield ")
-        print(" 2)  Berries ")
-        print(" 3)  Food")
-        print(" 4)  Heal Potion ")
-        print(" 5)  Lucky Egg ")
-        print(" 6)  Pokeball (Normal)")
-        time.sleep(y)
-        purchase = input("Enter what you would like to purchase ")
+        print("-------\n")
+        from items import Item
+        time.sleep(1.5)
+        purchase = input("Enter what you would like to purchase: ")
         print(purchase)
     else:
         x = True    
         while x == True:
-            q = input("Would you like to continue shopping? Y/N: ")
+            q = input("Would you like to continue or battle? Y or 'Battle': ")
             if (q== "Y" or q== "Yes" or q=="y" or q=="yes"):
                 x = False
                 return Go_Shop()
+            if (q== "Battle" or q== "battle" or q=="BATTLE" or q=="b"):
+                x = False
+                from battle import Battle
+                print(Battle)
 
     if purchase in Ability_Shield:
-        print("You have selected an Ability Shield")
-        print("     Use: A one time use item, Helpful for defense in pokemon battles. It counters any attack with the opposing. ")
+        print("-------\nYou have selected an Ability Shield")
+        print("     Use: A one time use item, Helpful for defense in pokemon battles. It counters any attack with the opposing.")
         print("     Amount: 25 coins ")
         user_request2 = input("     Would you like to purchase this item? ")
         if user_request2.upper() in Y:
@@ -47,31 +43,39 @@ def Go_Shop():
     else:
         x = True    
         while x == True:
-            q = input("Would you like to continue? Y/N: ")
+            q = input("Would you like to continue or battle? Y or 'Battle': ")
             if (q== "Y" or q== "Yes" or q=="y" or q=="yes"):
                 x = False
                 return Go_Shop()
+            if (q== "Battle" or q== "battle" or q=="BATTLE" or q=="b"):
+                x = False
+                from battle import Battle
+                print(Battle)
 
     
     if purchase in Berries:
-        print("You have selected berries ")
-        print("     Use: Berries are a helpful source of healing for your pokemon, improving the overall stats. ")
-        print("     Amount: 25 coins ")
+        print("-------\nYou have selected berries ")
+        print("     Use: Berries are a helpful source of healing for your pokemon, improving the overall stats.")
+        print("     Amount: 5 coins ")
         user_request3 = input("     Would you like to purchase this item? ")
         if user_request3.upper() == Y:
-            print(f"Current coins amount is", coins - 25 )
+            print(f"Current coins amount is", coins - 5 )
     else:
         x = True    
         while x == True:
-            q = input("Would you like to continue? Y/N: ")
+            q = input("Would you like to continue or battle? Y or 'Battle': ")
             if (q== "Y" or q== "Yes" or q=="y" or q=="yes"):
                 x = False
                 return Go_Shop()
+            if (q== "Battle" or q== "battle" or q=="BATTLE" or q=="b"):
+                x = False
+                from battle import Battle
+                print(Battle)
 
  
     if purchase in Food:
-        print("You have selected food")
-        print("     Use: Food is good for maintaing  ")
+        print("-------\nYou have selected food")
+        print("     Use: Food is good for maintaing and boosting the stats of your Pokemon.")
         print("     Amount: 25 coins ")
         user_request4 = input("     Would you like to purchase this item? ")
         if user_request4.upper() == Y:
@@ -79,30 +83,38 @@ def Go_Shop():
     else:
         x = True    
         while x == True:
-            q = input("Would you like to continue? Y/N: ")
+            q = input("Would you like to continue or battle? Y or 'Battle': ")
             if (q== "Y" or q== "Yes" or q=="y" or q=="yes"):
                 x = False
                 return Go_Shop()
+            if (q== "Battle" or q== "battle" or q=="BATTLE" or q=="b"):
+                x = False
+                from battle import Battle
+                print(Battle)
 
 
     if purchase in Heal_Potion:
-        print("You have selected a Heal Potion")
-        print("     Use: Heal potion is a good item for battling. When your pokemon is low on health, provide this delicious drink for them and their health increase ")
-        print("     Amount: 45 coins ")
+        print("-------\nYou have selected a Heal Potion")
+        print("     Use: Heal potion is a good item for battling. When your pokemon is low on health, provide this delicious drink for them and their health increase.")
+        print("     Amount: 15 coins ")
         user_request5 = input("     Would you like to purchase this item? ")
         if user_request5.upper() == Y:
-            print(f"Current coins amount is", coins - 45 )
+            print(f"Current coins amount is", coins - 15)
     else:
         x = True    
         while x == True:
-            q = input("Would you like to continue? Y/N: ")
+            q = input("Would you like to continue or battle? Y or 'Battle': ")
             if (q== "Y" or q== "Yes" or q=="y" or q=="yes"):
                 x = False
                 return Go_Shop()
+            if (q== "Battle" or q== "battle" or q=="BATTLE" or q=="b"):
+                x = False
+                from battle import Battle
+                print(Battle)
 
     if purchase in Lucky_Egg:
-        print("You have selected a Lucky Egg ")
-        print("     Use: Good for beginner Trainees. This special tiems helps boost XP  ")
+        print("-------\nYou have selected a Lucky Egg ")
+        print("     Use: Good for beginner Trainees. This special tiems helps boost XP.")
         print("     Amount: 30 coins ")
         user_request6 = input("     Would you like to purchase this item? ")
         if user_request6.upper() == Y:
@@ -110,14 +122,18 @@ def Go_Shop():
     else:
         x = True    
         while x == True:
-            q = input("Would you like to continue? Y/N: ")
+            q = input("Would you like to continue or battle? Y or 'Battle': ")
             if (q== "Y" or q== "Yes" or q=="y" or q=="yes"):
                 x = False
                 return Go_Shop()
+            if (q== "Battle" or q== "battle" or q=="BATTLE" or q=="b"):
+                x = False
+                from battle import Battle
+                print(Battle)
 
     if purchase in Pokeball:
-        print("You have selected a Pokeball ")
-        print("     Use: Pokeballs are the basic need for catching Pokemons. Pokeballs(Normal) increase your chance of catching one. ")
+        print("-------\nYou have selected a Pokeball ")
+        print("     Use: Pokeballs are the basic need for catching Pokemons. Pokeballs(Normal) increase your chance of catching one.")
         print("     Amount: 25 coins ")
         user_request7 = input("     Would you like to purchase this item? ")
         if user_request7.upper() == Y:
@@ -125,9 +141,13 @@ def Go_Shop():
     else:
         x = True    
         while x == True:
-            q = input("Would you like to continue? Y/N: ")
+            q = input("Would you like to continue or battle? Y or 'Battle': ")
             if (q== "Y" or q== "Yes" or q=="y" or q=="yes"):
                 x = False
                 return Go_Shop()
+            if (q== "Battle" or q== "battle" or q=="BATTLE" or q=="b"):
+                x = False
+                from battle import Battle
+                print(Battle)
 
 Go_Shop()
